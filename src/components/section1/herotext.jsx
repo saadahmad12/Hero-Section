afc1ad9f245c1d130c431117a36493c4731f8d30
saadahmad12 <saad01ahmad@gmail.com>
2026-02-25 11:35:49 +0500

    new Update

--- src/components/section1/herotext.jsx
+++ src/components/section1/herotext.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroText =()=>{
 
     return(
-        <div className='p-6 m-1'>
+        <div className='p-6 '>
         <h3 className='mb-7 text-5xl  font-bold'>Prospective <br/><span > customer  </span> <br/> segment </h3>
         <p className='text-l font-medium text-gray-500'>Hey This is Saad Ahmad . How are You and What Are You doing and how Your Project is Going on I Hope soo Ypu like my Project Based On Website</p>
     </div>
